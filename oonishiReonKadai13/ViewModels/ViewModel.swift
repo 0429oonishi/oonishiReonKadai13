3fad68a5389080578d031273829ca3b3e29ce752
0429oonishi <oonishireon110@gmail.com>
2021-08-04 10:23:54 +0900

    準備

--- oonishiReonKadai13/ViewModels/ViewModel.swift
+++ oonishiReonKadai13/ViewModels/ViewModel.swift
@@ -5,5 +5,29 @@
 //  Created by 大西玲音 on 2021/08/04.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
 
+protocol ViewModelInput {
+    
+}
+
+protocol ViewModelOutput: AnyObject {
+    
+}
+
+protocol ViewModelType {
+    var inputs: ViewModelInput { get }
+    var outputs: ViewModelOutput { get }
+}
+
+final class ViewModel: ViewModelInput, ViewModelOutput {
+    
+    
+    
+}
+
+extension ViewModel: ViewModelType {
+    var inputs: ViewModelInput { return self }
+    var outputs: ViewModelOutput { return self }
+}
